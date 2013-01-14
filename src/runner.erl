5f789aefbc303c49022427beaf7bbac558dd087b
Paul Peregud <paulperegud@gmail.com>
2013-01-14 15:12:40 +0100

    implement sync operations

--- src/runner.erl
+++ src/runner.erl
@@ -2,7 +2,12 @@
 
 -export([spawn/1,
 
-         exec/2]).
+         exec/2, exec/3,
+
+         exec_stream_sync/4,
+
+         exec_aggregate/2, exec_aggregate/3,
+         exec_aggregate_sync/2]).
 
 -record(runner, {owner, port, opts}).
 -define(FMT(Str, Args), lists:flatten(io_lib:format(Str, Args))).
@@ -11,57 +16,116 @@ spawn(Opts) ->
     Dir = proplists:get_value(cd, Opts, element(2, file:get_cwd())),
     Env = proplists:get_value(env, Opts, []),
 
-    ActualCmd = "/bin/sh -s unix:cmd",
+    ActualCmd = "/bin/sh -s unix:runner",
     Owner = self(),
     {Fun, Acc} = case proplists:get_value(fold, Opts) of
                      {_,_} = X -> X;
                      undefined ->
-                         {fun(X, A) ->
-                                  stream_to(Owner, X, A)
-                          end,
-                          []}
+                         {fun(_, _) -> ok end, []}
                  end,
-    
+
     spawn_link(fun() ->
                        Port = open_port({spawn, ActualCmd}, [{cd, Dir}, {env, Env},
                                                              exit_status, {line, 16384}, binary,
                                                              use_stdio, stderr_to_stdout]),
+                       link(Port),
+                       %% port_command(Port, "set -e"),
                        loop(#runner{port = Port, owner = Owner, opts = Opts}, Fun, Acc)
                end).
 
-stream_to(Owner, eoc, A) ->
-    Owner ! {eoc, self(), lists:reverse(A)},
-    [];
-stream_to(_Owner, {line, L}, A) ->
-    [L | A];
-stream_to(_Owner, {more, L}, A) ->
-    [L | A];
-stream_to(_Owner, done, Acc) ->
-    [] = Acc,
-    ok;
-stream_to(_Owner, {error, Rc}, A) ->
-    error({Rc, lists:reverse(A)}).
+cmd_aggregate(Owner, X, A) ->
+    case X of
+        {eoc, Error} ->
+            Owner ! {eoc, self(), Error, lists:reverse(A)},
+            [];
+        {line, L} ->
+            [L | A];
+        {more, L} ->
+            [L | A];
+        done ->
+            [];
+        {error, Rc} ->
+            error({Rc, lists:reverse(A)})
+    end.
 
 exec(Pid, Cmd) ->
     Pid ! {cmd, Cmd}.
-    
+
+exec(Pid, Cmd, Fold) when is_function(Fold) ->
+    Pid ! {cmd, Cmd, {Fold, []}};
+exec(Pid, Cmd, {F, A}) ->
+    Pid ! {cmd, Cmd, {F, A}}.
+
+exec_aggregate_sync(Pid, Cmd) ->
+    exec_aggregate(Pid, Cmd, self()),
+    receive
+        {eoc, Pid, 0, R} ->
+            {ok, R};
+        {eoc, Pid, Error, R} ->
+            {error, {Error, R}}
+    end.
+
+exec_aggregate(Pid, Cmd) ->
+    exec_aggregate(Pid, Cmd, self()).
+exec_aggregate(Pid, Cmd, Owner) ->
+    Fold = fun(X, A) ->
+                   cmd_aggregate(Owner, X, A)
+           end,
+    exec(Pid, Cmd, Fold).
+
+
+
+
+cmd_stream_to(Owner, Ref, Parent, X, A) ->
+    case X of
+        {eoc, Error} ->
+            Owner ! {eoc, self(), Error, lists:reverse(A)}, [];
+        {line, L} ->
+            Parent ! {Ref, line, L}, [];
+        {more, L} ->
+            Parent ! {Ref, line, L}, [];
+        done ->
+            A;
+        {error, Rc} ->
+            error({Rc, lists:reverse(A)})
+    end.
+
+exec_stream_sync(Pid, Cmd, Ref, Parent) ->
+    Owner = self(),
+    Send = fun(X, A) ->
+                   cmd_stream_to(Owner, Ref, Parent, X, A)
+           end,
+    exec(Pid, Cmd, Send),
+    receive
+        {eoc, Pid, 0, R} ->
+            {ok, R};
+        {eoc, Pid, Error, R} ->
+            {error, {Error, R}}
+    end.
+
+
 loop(#runner{port = Port} = R, Fun, Acc) ->
     receive
         {cmd, Cmd} ->
             port_command(Port, mk_cmd(Cmd)),
             loop(R, Fun, Acc);
-        {Port, {data, {eol, <<4,4,4,4>>}}} ->
-            loop(R, Fun, Fun(eoc, Acc));            
+        {cmd, Cmd, {F, A}} ->
+            port_command(Port, mk_cmd(Cmd)),
+            loop(R, F, A);
+        {Port, {data, {eol, <<4,4,4,4, Error/binary>>}}} ->
+            loop(R, Fun, rep(Fun, {eoc, list_to_integer(binary_to_list(Error))}, Acc));
         {Port, {data, {eol, Line}}} ->
-            loop(R, Fun, Fun({line, <<Line/binary, "\n">>}, Acc));
+            loop(R, Fun, rep(Fun, {line, <<Line/binary, "\n">>}, Acc));
         {Port, {data, {noeol, Line}}} ->
-            loop(R, Fun, Fun({more, Line}, Acc));
+            loop(R, Fun, rep(Fun, {more, Line}, Acc));
         {Port, {exit_status, 0}} ->
-            Fun(done, Acc);
+            rep(Fun, done, Acc);
         {Port, {exit_status, Rc}} ->
-            Fun({error, Rc}, Acc)
+            rep(Fun, {error, Rc}, Acc)
     end.
-    
+
+rep(Fun, X, A) ->
+    Fun(X, A).
+
 mk_cmd(Cmd) ->
-    io_lib:format("~s;\n echo  \"\^D\^D\^D\^D\"\n", [Cmd]).
-    
+    binary_to_list(iolist_to_binary(io_lib:format("~s;\n echo  \"\^D\^D\^D\^D$?\"\n", [Cmd]))).
